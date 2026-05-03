6e5b0d1c0668eba2f6168396c6c34893b0b84a94
albarrab <albarrakii@hotmail.com>
2026-05-03 10:06:51 +0100

    Delete system_code directory

--- system_code/web/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DB_HOST = getenv('DB_HOST') ?: 'localhost';
-$DB_PORT = getenv('DB_PORT') ?: '3306';
-$DB_NAME = getenv('DB_NAME') ?: 'mdssyste_network';
-$DB_USER = getenv('DB_USER') ?: 'mdssyste_bader';
-$DB_PASS = getenv('DB_PASS') ?: 'Bader@2026';
-$ML_API_URL = getenv('ML_API_URL') ?: 'https://bader.mdscodeforge.co.uk/ml';
